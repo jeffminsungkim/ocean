eb04f61dff7c89d905aff9cd40890f35234dcc45
JeffMinsungKim <ziznzkak@gmail.com>
2018-03-17 05:44:22 +0900

    Fix bubble sort

--- languages/javascript/algorithms/bubbleSort.js
+++ languages/javascript/algorithms/bubbleSort.js
@@ -9,12 +9,12 @@ const pushRandomNumbers = () => {
 };
 
 const bubbleSort = (arr) => {
-  const len = arr.length;
-  for (let i = 0; i < len - 1; i++) {
-    for (let j = i + 1; j < len; j++) {
-      if (arr[i] > arr[j]) {
-        let temp = arr[i];
-        arr[i] = arr[j];
+	const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 1; j < len - i; j++) {
+      if (arr[j] < arr[j - 1]) {
+        let temp = arr[j - 1];
+        arr[j - 1] = arr[j];
         arr[j] = temp;
       }
     }
