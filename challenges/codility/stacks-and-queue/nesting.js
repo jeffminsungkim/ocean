b0b72e94db83b6c97b9cfbf205196e99d5d285c5
JeffMinsungKim <ziznzkak@gmail.com>
2018-03-21 16:54:09 +0900

    challenges(codility): add lesson 7 Nesting in JS

---
+++ challenges/codility/stacks-and-queue/nesting.js
@@ -0,0 +1,13 @@
+// Details: https://app.codility.com/programmers/lessons/7-stacks_and_queues/nesting/
+
+function solution(S) {
+  const shape = { ')': '(' };
+  const stack = [];
+
+  for (let parenthesis of S) {
+    if (stack.length > 0 && shape[parenthesis] === stack[stack.length - 1]) stack.pop();
+    else stack.push(parenthesis);
+  }
+  return stack.length === 0 ? 1 : 0;
+}
+
