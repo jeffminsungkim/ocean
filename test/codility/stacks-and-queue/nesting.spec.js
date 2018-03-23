2b7dc298b005e5d3d6a7c328e5b226841efbf814
JeffMinsungKim <ziznzkak@gmail.com>
2018-03-24 01:37:37 +0900

    test(codility): add test function

---
+++ test/codility/stacks-and-queue/nesting.spec.js
@@ -0,0 +1,15 @@
+const assert = require('chai').assert;
+const solution = require("../../../challenges/codility/stacks-and-queue/nesting");
+
+describe("Nesting: Determine whether a given string of parentheses is properly nested", () => {
+  const properlyNestedStr = "(()(())())";
+  const improperlyNestedStr = "())";
+
+  it(`should return 1 if a given string properly nested like ${properlyNestedStr}`, () => {
+    assert.equal(solution(properlyNestedStr), 1);
+  });
+
+  it(`should return 0 if a given string improperly nested like ${improperlyNestedStr}`, () => {
+    assert.equal(solution(improperlyNestedStr), 0);
+  });
+});
